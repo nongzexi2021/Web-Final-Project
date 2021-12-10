12e321cb66b6de3bfc3cbfa6c551c5456b098031
siyu3 <meikoshans@gmail.com>
2021-12-10 12:24:55 -0800

    profile update

--- src/App.js
+++ src/App.js
@@ -9,18 +9,28 @@ import DetailsScreen from "./Detail/DetailsScreen";
 import SignBar from "./SignBar/SignBar";
 import HomeScreen from "./Home/HomeScreen";
 import CartScreen from "./Cart/CartScreen";
-
+import Profile from'./Profile/main';
+import EditProfile from './EditProfile/index'
+import './profile.css'
+import {Provider} from "react-redux";
+import {combineReducers, createStore} from "redux";
+import profiles from "./reducer/profile";
+const reducer = combineReducers({profile: profiles})
+const store = createStore(reducer);
 function App() {
     return (
         <BrowserRouter>
+            <Provider store={store}>
             <Routes>
                 {/*<Route path="/" element={<SearchScreen/>}/>*/}
                 {/*<Route path="/:searchTerm" element={<SearchScreen/>}/>*/}
                 {/*<Route path="/details/:id" element={<DetailsScreen/>}/>*/}
                 <Route path = "/" element={<HomeScreen/>}/>
                 <Route path = "/ebay/cart" element={<CartScreen/>}/>
-
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/editProfile" element={<EditProfile />} />
             </Routes>
+            </Provider>
 
 </BrowserRouter>
 
